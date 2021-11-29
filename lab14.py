4d423de44fb58c90caae72a36292199a5fcc06cb
ashaa9121 <ashaa9121@gmail.com>
2021-11-30 00:43:48 +0530

    combined two files

--- lab14.py
+++ lab14.py
@@ -3,4 +3,4 @@
 with open("file1.txt") as f1:
     with open("file2.txt") as f2:
         for line1,line2 in zip(f1,f2):
-            print(line1 +" "+ line2)
+            print("Combined files : ",line1 +" "+ line2)
