1836c855df20585a63ac1287067595aea1d14502
ashaa9121 <ashaa9121@gmail.com>
2021-11-30 00:40:08 +0530

    combine lines

---
+++ lab14.py
@@ -0,0 +1,6 @@
+# Program to combine each line from first file with the corresponding line in second file.
+
+with open("file1.txt") as f1:
+    with open("file2.txt") as f2:
+        for line1,line2 in zip(f1,f2):
+            print(line1 +" "+ line2)
