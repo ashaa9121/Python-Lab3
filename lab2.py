923cb6c391ad4975e364cce09db03accb199cc50
ashaa9121 <ashaa9121@gmail.com>
2021-11-29 13:01:56 +0530

    content added

---
+++ lab2.py
@@ -0,0 +1,6 @@
+# Write a Python program to read first n lines of a file.
+
+number_of_lines = int(input("Enter no: of lines you want to read: "))
+f = open("content.txt")
+for i in range(number_of_lines):
+    print(f.readline())
